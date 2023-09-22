df029c5a6a9714a742f1d06d96241e6db00a6abc
Jinhyun Choi <jinhyun.choi@digipen.edu>
2023-09-22 16:52:06 -0700

    Update Root Namespace and Add Mesh Cutter

---
+++ GAM400/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paparazzi
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public Vector2 moveInput { get; private set; }
+        public bool isJump { get; private set; }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            if (moveInput.sqrMagnitude > 1f)
+            {
+                moveInput = moveInput.normalized;
+            }
+            isJump = Input.GetButtonDown("Jump");
+        }
+    }
+}
