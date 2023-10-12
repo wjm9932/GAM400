6bfa40982f16b9e17cc710e56d70ccb7ee7477bb
Jinhyun Choi <jinhyun.choi@digipen.edu>
2023-10-12 09:58:40 -0700

    Finish to make magic camera for prototype

--- GAM400/Assets/Scripts/Utility.cs
+++ GAM400/Assets/Scripts/Utility.cs
@@ -1,17 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Paparazzi
 {
-    public static class Utility
+    public class Utility
     {
-        public static void DestroyChildren(this Transform root)
+        public static void DestroyChildren(Transform root)
         {
             foreach (Transform child in root.transform)
             {
                 UnityEngine.Object.Destroy(child.gameObject);
             }
+        }
+
+        public static Vector3 Plane3Intersect(Plane p1, Plane p2, Plane p3)
+        {
+            return ((-p1.distance * Vector3.Cross(p2.normal, p3.normal)) +
+                    (-p2.distance * Vector3.Cross(p3.normal, p1.normal)) +
+                    (-p3.distance * Vector3.Cross(p1.normal, p2.normal))) /
+                   (Vector3.Dot(p1.normal, Vector3.Cross(p2.normal, p3.normal)));
+        }
+
+        public static GameObject[] GetViewingObject(Plane[] planes)
+        {
+            List<GameObject> lists = new List<GameObject>();
+            var slicables = GameObject.FindGameObjectsWithTag("Sliceable");
+            foreach (var obj in slicables)
+            {
+                var renderer = obj.GetComponent<Renderer>();
+
+                var result = GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+
+                if (result)
+                    lists.Add(obj);
+            }
+
+            return lists.ToArray();
         }
     }
 }
