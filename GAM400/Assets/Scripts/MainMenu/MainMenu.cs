9715ddee84450c3f3e1680347f755564fd894aca
jae <wod6983@naver.com>
2023-10-13 22:25:26 -0700

    Menu
    Make MainMenu PauseMenu

---
+++ GAM400/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void OnClickOption()
+    {
+        SceneManager.LoadScene("Test_PhotoFeature");
+    }
+    public void OnClickEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
