5a329fd0112513f809aeb6ca2a997e4c775285e3
Hyosang Jeong <70678504+Hyosang-Jeong@users.noreply.github.com>
2023-11-12 14:54:53 -0800

    Toon rendering

---
+++ GAM400/Assets/Scripts/Update Shader Properties.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paparazzi
+{
+[ExecuteInEditMode]
+    public class UpdateShaderProperties : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            if (gameObject.transform.hasChanged)
+            {
+                Renderer[] renderers = GameObject.FindObjectsOfType<Renderer>();
+                foreach( var r in renderers)
+                {
+                    Material m;
+#if UNITY_EDITOR
+                    m = r.sharedMaterial; 
+#else
+                    m = r.material;
+#endif
+                    if(string.Compare(m.shader.name,"Shader Graphs/ToonRamp") == 0)
+                    {
+                        m.SetVector("_LightDir", transform.forward);
+                    }
+                }
+            }
+        }
+    }
+}
